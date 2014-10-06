57be40236e095b11d0300214670fdce7f5f80046
Aunik Hussain <31704@longroad.ac.uk>
2014-10-06 11:24:02 +0100

    coomit

---
+++ psuedo ex 2.py
@@ -0,0 +1,7 @@
+#Aunik Hussain
+#30-09-2014
+#ASCII page 2 pseudo code ex 2
+
+#chr(MyInt) Takes an integer as a parameter and returns the equivalent ASCII character.
+#ord(MyChar) Takes a character as a parameter and returns the equivalent ASCII code.
+
